bb63099547c7822470b8bae9cca7985476056d22
Adam Scott <ascott.ca@gmail.com>
2016-05-28 22:03:54 -0400

    Add main.py and app.py.

---
+++ genial/app.py
@@ -0,0 +1,9 @@
+'''
+Created on 28 mai 2016
+
+@author: Adam Scott
+'''
+
+def run():
+  print("Hello, World.")
+  
---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Created on 28 mai 2016
+
+@author: Adam Scott
+'''
+
+if __name__ == '__main__':
+    import sys
+    from genial import app
+    sys.exit(app.run())
