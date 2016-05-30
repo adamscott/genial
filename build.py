a61b7f3ca7d9f40c0628e731e7b106e0b84d9805
Adam Scott <ascott.ca@gmail.com>
2016-05-30 18:25:49 -0400

    First version of the build.py file.

--- build.py
+++ build.py
@@ -1,5 +1,26 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import init, use_plugin, task, description, Project, Logger
 
-use_plugin('python.core')
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.coverage")
+use_plugin("python.install_dependencies")
+use_plugin("python.distutils")
+use_plugin("python.pydev")
+use_plugin("python.pycharm")
+use_plugin("exec")
 
-default_task = 'publish'
+default_task = "publish"
+
+@init
+def initialize(project:Project):
+    project.build_depends_on('mockito')
+
+@task
+@description("Compiles .ui files using pyuic5")
+def compile_ui(project:Project, logger:Logger):
+    logger.debug("Compiling ui.")
+
+@task
+@description("Compiles .qrc files using pyrcc5")
+def compile_qrc(project:Project, logger:Logger):
+    logger.debug("Compiling qrc.")
