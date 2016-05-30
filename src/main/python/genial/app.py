00ee4dc06b1e83108f3b50e1d8a56d6bbd94c8d1
Adam Scott <ascott.ca@gmail.com>
2016-05-30 18:25:22 -0400

    Add a new blank line to respect Python guidelines.

--- src/main/python/genial/app.py
+++ src/main/python/genial/app.py
@@ -6,14 +6,14 @@
     :copyright: (c) 2015, Adam Scott.
     :license: GPL3, see LICENSE for more details.
 """
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
 
 from genial.mainwindow import MainWindow
 
 
 def run():
     import sys
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     return app.exec_()
