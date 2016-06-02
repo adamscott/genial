b4c28110818eb5e993fffbd5f5c7d632fe8b5a2f
Adam Scott <ascott.ca@gmail.com>
2016-06-02 11:32:53 -0400

    Add new parameters to app.py
    * App name
    * App display name
    * HdPi display support

--- src/main/python/genial/app.py
+++ src/main/python/genial/app.py
@@ -6,7 +6,7 @@
     :copyright: (c) 2015, Adam Scott.
     :license: GPL3, see LICENSE for more details.
 """
-from PyQt5.QtCore import QCoreApplication, QTranslator, QLocale
+from PyQt5.QtCore import QCoreApplication, QTranslator, QLocale, Qt
 from PyQt5.QtWidgets import QApplication
 
 from genial.mainwindow import MainWindow
@@ -15,6 +15,9 @@ from genial.mainwindow import MainWindow
 def run():
     import sys
     app = QApplication(sys.argv)
+    app.setApplicationName("Génial")
+    app.setApplicationDisplayName("Génial")
+    app.setProperty("AA_EnableHighDpiScaling", True)
     translator = QTranslator()
     if translator.load(QLocale(), "genial", "_", ":/locale"):
         # noinspection PyArgumentList,PyCallByClass,PyTypeChecker
