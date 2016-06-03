bd3e1ecbd0dd2c64b8ac395f4fc56d371c9cc562
Adam Scott <ascott.ca@gmail.com>
2016-06-03 00:11:58 -0400

    Add missing credits in controllers' and services' __init__.py

---
+++ src/main/python/genial/controllers/__init__.py
@@ -0,0 +1,11 @@
+"""
+    Génial
+    ================================================
+    A "Génies en herbe" questions manager.
+
+    :copyright: (c) 2015, Adam Scott.
+    :license: GPL3, see LICENSE for more details.
+"""
+__author__ = 'Adam Scott'
+__author_email__ = "ascott.ca@gmail.com"
+__license__ = 'GPL3'
