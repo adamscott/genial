bec2bd3a4023f2eacfbce2a3e33868de93c93f33
Adam Scott <ascott.ca@gmail.com>
2016-06-03 18:30:58 -0400

    Add base Qt translations.

--- src/main/python/genial/app.py
+++ src/main/python/genial/app.py
@@ -6,24 +6,33 @@
     :copyright: (c) 2015, Adam Scott.
     :license: GPL3, see LICENSE for more details.
 """
-from PyQt5.QtCore import QCoreApplication, QTranslator, QLocale, Qt
+from PyQt5.QtCore import QCoreApplication, QTranslator, QLocale, QLibraryInfo
 from PyQt5.QtWidgets import QApplication
 
 from genial.controllers.maincontroller import MainController
 
+
 def run():
     import sys
+
     app = QApplication(sys.argv)
     app.setApplicationName("Génial")
     app.setApplicationDisplayName("Génial")
     app.setProperty("AA_EnableHighDpiScaling", True)
-    translator = QTranslator()
-    if translator.load(QLocale(), "genial", "_", ":/locale"):
+
+    # Qt translation
+    qt_translator = QTranslator()
+    if qt_translator.load(QLocale(), "qt", "_", ":/locale"):
         # noinspection PyArgumentList,PyCallByClass,PyTypeChecker
-        QCoreApplication.installTranslator(translator)
+        QCoreApplication.installTranslator(qt_translator)
+
+    # App translation
+    genial_translator = QTranslator()
+    if genial_translator.load(QLocale(), "genial", "_", ":/locale"):
+        # noinspection PyArgumentList,PyCallByClass,PyTypeChecker
+        QCoreApplication.installTranslator(genial_translator)
 
     main_controller = MainController(app)
     main_controller.start()
 
-    return app.exec_()
-
+    return app.exec()
