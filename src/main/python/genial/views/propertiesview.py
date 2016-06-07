d178766e8ca841e6dd362911639b8a95febdaabd
Adam Scott <ascott.ca@gmail.com>
2016-06-07 13:11:26 -0400

    PropertiesView is now a QDialog instead of a QWidget.

--- src/main/python/genial/views/propertiesview.py
+++ src/main/python/genial/views/propertiesview.py
@@ -6,13 +6,13 @@
     :copyright: (c) 2015, Adam Scott.
     :license: GPL3, see LICENSE for more details.
 """
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QDialog
 from genial.views.gen.ui_propertiesview import Ui_PropertiesView
 
 
-class PropertiesView(QWidget, Ui_PropertiesView):
+class PropertiesView(QDialog, Ui_PropertiesView):
     def __init__(self,parent=None):
-        QWidget.__init__(self, parent)
+        QDialog.__init__(self, parent)
         self.ui = Ui_PropertiesView()
         self.ui.setupUi(self)
         self.ui.retranslateUi(self)
