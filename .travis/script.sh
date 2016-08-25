e6f88c1fcc40ddb56cac6000cc083dd9b433414a
Adam Scott <ascott.ca@gmail.com>
2016-08-25 13:12:31 -0400

    Add py.test test to test the test.

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,4 +8,7 @@ pyenv global 3.5.2
 LRELEASE=/opt/qt57/bin/lrelease
 
 python setup.py bootstrap --lrelease=$LRELEASE
+echo "===   TEST   ==="
+/home/travis/.pyenv/versions/3.5.2/bin/py.test --cov=genial/ tests/
+echo "=== END TEST ==="
 python setup.py test
