2dcefd60a917ac2d88fee1028c9cddbbd5c32d47
Adam Scott <ascott.ca@gmail.com>
2016-09-25 15:08:47 -0400

    Remove pyenv installation on Travis (Linux)

--- .travis/pyenv.sh
+++ .travis/pyenv.sh
@@ -3,14 +3,8 @@ set -ex
 
 if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then # OS X
     brew upgrade pyenv
-
-else # Linux
-    echo "test pyenv?"
-    ls -a ~ # test if pyenv is already there?
-    # Install pyenv
-    wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-    export PATH="/home/travis/.pyenv/bin:$PATH"
 fi
+# pyenv is already installed in the Linux container
 
 eval "$(pyenv init -)"
 pyenv install 3.5.2
