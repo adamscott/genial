5284f2d07ff9df9fb52eafe0579c0cd3020d0356
Adam Scott <ascott.ca@gmail.com>
2016-09-26 18:51:12 -0400

    Update .travis/pyenv.sh
    * Add comments
    * Add flag to make the build faster
    * Add pyenv-virtualenv init
    * Set verbose to 3.5.2 installation

--- .travis/pyenv.sh
+++ .travis/pyenv.sh
@@ -8,15 +8,24 @@ fi
 # Unified pyenv install
 wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
+# Add ~/.pyenv to $PATH
 if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then # OS X
     export PATH="/Users/travis/.pyenv/bin:$PATH"
 else # Linux
     export PATH="/home/travis/.pyenv/bin:$PATH"
 fi
 
+# Makes builds a little faster
+export PYTHON_CFLAGS='-O2'
+
+# Inits pyenv
 eval "$(pyenv init -)"
-pyenv install 3.5.2
+eval "$(pyenv virtualenv-init -)"
+
+# Installs and sets global python to v3.5.2
+pyenv install --verbose 3.5.2
 pyenv global 3.5.2
 
+# Get the latest version of pip and setuptools, which both come preinstalled, but outdated
 pip install --upgrade pip
 pip install --upgrade setuptools
