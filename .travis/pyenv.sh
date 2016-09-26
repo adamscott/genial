14ac635e3b7b6c95d9ffb14a724f6378fafc2937
Adam Scott <ascott.ca@gmail.com>
2016-09-26 15:47:57 -0400

    Fix pyenv install path for both Linux and OSX (Travis)

--- .travis/pyenv.sh
+++ .travis/pyenv.sh
@@ -7,7 +7,12 @@ fi
 
 # Unified pyenv install
 wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-export PATH="/home/travis/.pyenv/bin:$PATH"
+
+if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then # OS X
+    export PATH="/Users/travis/.pyenv/bin:$PATH"
+else # Linux
+    export PATH="/home/travis/.pyenv/bin:$PATH"
+fi
 
 eval "$(pyenv init -)"
 pyenv install 3.5.2
