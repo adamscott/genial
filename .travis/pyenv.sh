b1f0f6f564e53d0a057c327dd8157d3283b80d4b
Adam Scott <ascott.ca@gmail.com>
2016-09-26 14:33:27 -0400

    Install (again) pyenv on Linux build (Travis)

--- .travis/pyenv.sh
+++ .travis/pyenv.sh
@@ -3,8 +3,12 @@ set -ex
 
 if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then # OS X
     brew upgrade pyenv
+
+else # Linux
+    # Install pyenv
+    wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+    export PATH="/home/travis/.pyenv/bin:$PATH"
 fi
-# pyenv is already installed in the Linux container
 
 eval "$(pyenv init -)"
 pyenv install 3.5.2
