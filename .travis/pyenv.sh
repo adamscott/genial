c0867528490f25bb4e4a0730cc533789dbc34bd5
Adam Scott <ascott.ca@gmail.com>
2016-09-26 15:26:19 -0400

    Replace brew pyenv by an unified install (Travis)
    The (brew) pyenv installed was outdated.

--- .travis/pyenv.sh
+++ .travis/pyenv.sh
@@ -2,13 +2,12 @@
 set -ex
 
 if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then # OS X
-    $(: do nothing, already installed)
-
-else # Linux
-    # Install pyenv
-    wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-    export PATH="/home/travis/.pyenv/bin:$PATH"
+    brew uninstall pyenv # uninstall outdated pyenv
 fi
+
+# Unified pyenv install
+wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+export PATH="/home/travis/.pyenv/bin:$PATH"
 
 eval "$(pyenv init -)"
 pyenv install 3.5.2
