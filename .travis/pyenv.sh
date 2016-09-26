afe5908e56e6d2cfe023e48639dd1cc402da4c1f
Adam Scott <ascott.ca@gmail.com>
2016-09-26 19:01:55 -0400

    Add gist support to python non-static build.

--- .travis/pyenv.sh
+++ .travis/pyenv.sh
@@ -22,9 +22,18 @@ export PYTHON_CFLAGS='-O2'
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
+# Python version to build
+python_version="3.5.2"
+
+# Log name
+python_build_log="python-${python_version}-${TRAVIS_OS_NAME}.log"
+
 # Installs and sets global python to v3.5.2
-pyenv install --verbose 3.5.2
-pyenv global 3.5.2
+pyenv install --verbose "${python_version}" &>"${python_build_log}"
+pyenv global "${python_version}"
+
+# Update gist
+gist -u 1519c3ffb6bcccdf40223563f3a84448 "${python_build_log}"
 
 # Get the latest version of pip and setuptools, which both come preinstalled, but outdated
 pip install --upgrade pip
