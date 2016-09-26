cc9ec5e5aada258ea22fa9be4f4b3cacffd009f8
Adam Scott <ascott.ca@gmail.com>
2016-09-26 18:27:52 -0400

    Add python dependencies for pyenv builds. (Travis)

--- .travis/setup_packages.sh
+++ .travis/setup_packages.sh
@@ -3,5 +3,8 @@ set -ex
 
 if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then # OS X
     brew update
+
+    # pyenv dependencies
+    brew install autoconf openssl pkg-config readline xz
 fi
-# No need to update packages in Linux build
+# No need to setup packages in Linux build
