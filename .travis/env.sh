2d5f6f2d7c04a291d3d8345df1546eb54e1bf368
Adam Scott <ascott.ca@gmail.com>
2016-09-27 19:22:33 -0400

    Add ~/.pyenv to PATH from env.sh

--- .travis/env.sh
+++ .travis/env.sh
@@ -1,3 +1,10 @@
+# Add ~/.pyenv to $PATH
+if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then  # OS X
+    export PATH="/Users/travis/.pyenv/bin:$PATH"
+else  # Linux
+    export PATH="/home/travis/.pyenv/bin:$PATH"
+fi
+
 # Sets $PATH to access Qt binaries
 export QT_INSTALL_DIR="$(echo ~)/qt"
 export PATH="${QT_INSTALL_DIR}/bin":$PATH
--- .travis/pyenv.sh
+++ .travis/pyenv.sh
@@ -8,13 +8,6 @@ fi
 # Unified pyenv install
 wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
-# Add ~/.pyenv to $PATH
-if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then # OS X
-    export PATH="/Users/travis/.pyenv/bin:$PATH"
-else # Linux
-    export PATH="/home/travis/.pyenv/bin:$PATH"
-fi
-
 # Makes builds a little faster
 export PYTHON_CFLAGS='-O2'
 
